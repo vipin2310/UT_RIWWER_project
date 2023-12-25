e327240043e70b152462413b0f2a34f056fe07ca
vipin2310 <vipinsingh2310@gmail.com>
2023-12-25 17:44:50 +0000

    Add docstrings and finalizing basic functionalities of loader.py

--- vierlinden/data/loader.py
+++ vierlinden/data/loader.py
@@ -2,18 +2,36 @@ import pandas as pd
 import numpy as np
 import logging
 from vierlinden.config import data_path, target_filename, sensor_filename
+from pytorch_forecasting import TimeSeriesDataSet
+from typing import Tuple
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 class DataProcessor:
-    def __init__(self, load_and_process=False):
+    """A class to process and prepare data for time series analysis.
+
+    Parameters
+    ----------
+    load_and_process : bool, optional
+        Flag to load and process data during initialization, by default False
+    """
+    
+    def __init__(self, load_and_process: bool = False):
+        """Constructor for DataProcessor."""
         
         self.processed = True if load_and_process else False
         self.data = self.load_processed_data() if load_and_process else None
 
-    def load_processed_data(self):
+    def load_processed_data(self) -> pd.DataFrame:
+        """Loads processed data if available, otherwise processes raw sensor and target data.
+
+        Returns
+        -------
+        pd.DataFrame
+            Processed data.
+        """
         
         if self.data is not None and self.processed:
             return self.data
@@ -24,7 +42,19 @@ class DataProcessor:
         
         return self.data
     
-    def prepare_for_target(self, target_col):
+    def prepare_for_target(self, target_col: str) -> pd.DataFrame:
+        """Prepares the data for modeling a specific target variable.
+
+        Parameters
+        ----------
+        target_col : str
+            Name of the target column.
+
+        Returns
+        -------
+        pd.DataFrame
+            Data prepared for the target variable.
+        """
         
         t = self.data.drop(['Entleerung_RüB', 'Füllstand_RüB_1', 'Füllstand_RüB_2', 'Füllstand_RüB_3'], axis=1)
         
@@ -41,23 +71,78 @@ class DataProcessor:
         
         return prediction_ready_data
     
-    def get_training_and_validation_timeseries_dataset(self, target_col, train_frac=0.8, num_time_series = 1):
+    def get_training_and_validation_timeseries_dataset(self, target_col: str, train_frac: float = 0.8, num_time_series: int = 1) -> Tuple[TimeSeriesDataSet, TimeSeriesDataSet]:
+        """Generates training and validation TimeSeriesDataSet for the specified target column.
+
+        Parameters
+        ----------
+        target_col : str
+            Target variable column name.
+        train_frac : float, optional
+            Fraction of data to be used for training, by default 0.8.
+        num_time_series : int, optional
+            Number of time series to create, by default 1.
+
+        Returns
+        -------
+        Tuple[TimeSeriesDataSet, TimeSeriesDataSet]
+            Tuple containing training and validation TimeSeriesDataSet.
+        """
         
         prediction_ready_data = self.prepare_for_target(target_col)
         data = self.__add_series_and_timeidx(prediction_ready_data, num_time_series)
         
-        # TODO Create dataloader
+        training_cutoff = int(round(data["time_idx"].max() * train_frac))
+        
+        # Parameters for TimeSeriesDataSet (setup by Teo)
+        max_encoder_length = 24*2
+        max_prediction_length = 5*2
+        context_length = max_encoder_length
+        prediction_length = max_prediction_length
+        
+        training = TimeSeriesDataSet(
+            data = data[lambda x: x.time_idx <= training_cutoff],
+            target_normalizer='auto',
+            time_idx='time_idx',
+            target=target_col,
+            group_ids=['series'],
+            time_varying_unknown_reals=list(set(data.columns) - {'Datetime', 'series', 'time_idx'}),
+            max_encoder_length=context_length,
+            min_encoder_length=max_encoder_length,
+            max_prediction_length=prediction_length,
+            min_prediction_length=max_prediction_length,
+            allow_missing_timesteps=True
+        )
+        
+        validation = TimeSeriesDataSet.from_dataset(training, data, min_prediction_idx=training_cutoff + 1)
+        
+        return training, validation
         
     
-    def export_data(self, output_path):
-        """Export data to a file."""
+    def export_data(self, output_path: str):
+        """Exports the processed data to a CSV file.
+
+        Parameters
+        ----------
+        output_path : str
+            Path to save the CSV file.
+        """
+        
         try:
             self.data.to_csv(output_path)
             logger.info(f"Data exported successfully to {output_path}")
         except Exception as e:
             logger.error(f"Error exporting data to {output_path}: {e}")
     
-    def __read_data(self):
+    def __read_data(self) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        """Reads sensor and target data from predefined file paths.
+
+        Returns
+        -------
+        Tuple[pd.DataFrame, pd.DataFrame]
+            Tuple of sensor and target data.
+        """
+        
         try:
             sensor_data = pd.read_csv(data_path / sensor_filename, sep=",")
             target_data = pd.read_csv(data_path / target_filename, sep=",")
@@ -68,7 +153,22 @@ class DataProcessor:
         except Exception as e:
             logger.error(f"Error loading data from {data_path}: {e}")
 
-    def __merge_data(self, sensor_data, target_data):
+    def __merge_data(self, sensor_data: pd.DataFrame, target_data: pd.DataFrame) -> pd.DataFrame:
+        """Merges sensor and target data on a common timestamp.
+
+        Parameters
+        ----------
+        sensor_data : pd.DataFrame
+            DataFrame containing sensor data.
+        target_data : pd.DataFrame
+            DataFrame containing target data.
+
+        Returns
+        -------
+        pd.DataFrame
+            Merged data.
+        """
+        
         try:
             # Convert to datetime type
             sensor_data['Datetime'] = pd.to_datetime(sensor_data['Datetime'])
@@ -81,7 +181,19 @@ class DataProcessor:
         except Exception as e:
             logger.error(f"Error merging data: {e}")
 
-    def __process_nan(self, all_data):
+    def __process_nan(self, all_data: pd.DataFrame) -> pd.DataFrame:
+        """Processes NaN values in the dataset.
+
+        Parameters
+        ----------
+        all_data : pd.DataFrame
+            The dataset with potential NaN values.
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataset with NaN values processed.
+        """
         
         # Fill NaN values in Niederschlage (rainfall) column
         all_data['Niederschlag'] = all_data['Niederschlag'].fillna(0)
@@ -103,7 +215,19 @@ class DataProcessor:
         
         return complete_data
     
-    def __remove_high_nan_cols(self, all_data):
+    def __remove_high_nan_cols(self, all_data: pd.DataFrame) -> pd.DataFrame:
+        """Removes columns with a high proportion of NaN values from the dataset.
+
+        Parameters
+        ----------
+        all_data : pd.DataFrame
+            The dataset to be processed.
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataset with high NaN columns removed.
+        """
         
         # Remove columns with high proportion of missing values
         data_with_removed_highnans = all_data.drop(
@@ -119,7 +243,21 @@ class DataProcessor:
         
         return data_with_removed_highnans
     
-    def __add_series_and_timeidx(self, data, num_time_series):
+    def __add_series_and_timeidx(self, data: pd.DataFrame, num_time_series: int) -> pd.DataFrame:
+        """Adds 'series' and 'timeidx' columns to the dataset for time series modeling.
+
+        Parameters
+        ----------
+        data : pd.DataFrame
+            The dataset to be processed.
+        num_time_series : int
+            The number of time series to divide the data into.
+
+        Returns
+        -------
+        pd.DataFrame
+            Dataset with 'series' and 'timeidx' columns added.
+        """
         
         # Assign timeidx to each row
         data['timeidx'] = data.index
